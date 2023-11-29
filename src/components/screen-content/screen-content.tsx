41363b89db42f5f3d0e582bbd11ea6c7829d6482
bodiabeliy <bboda2106@gmail.com>
2023-11-29 16:20:06 +0200

    show tablet text

--- src/components/screen-content/screen-content.tsx
+++ src/components/screen-content/screen-content.tsx
@@ -18,13 +18,13 @@ export const ScreenContent = component$<Props>(() => {
             AI Unity is UWP Digital's own development for everyday life and business
             </h3>
         </div>
-        <div class="flex  max-[768px]:hidden h-full text-center items-center justify-center ">
+        <div class="flex  max-[767px]:hidden h-full text-center items-center justify-center ">
           <div class="flex h-full z-10">
           <h1 class="text-6xl font-bold uppercase text-white mt-40">
             Your reliable digital <br /> marketing partner
             </h1>
           </div>
-            <h3 class=" text-lg absolute text-zinc-300 bottom-[150px] m-auto max-w-[663px] lg:mb-[180px]">
+            <h3 class=" text-lg absolute text-zinc-300 bottom-[150px] m-auto max-w-[663px]  lg:mb-[180px]">
             We are a full-service digital marketing agency focused on your success. We do not sell ideas. We sell a solution for your business needs.
             </h3>
         </div>
